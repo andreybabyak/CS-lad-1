d429d1d7bbc49f993fb925e4727961c9f1a2f712
andreybabyak <babyak8991@gmail.com>
2019-03-19 14:36:50 +0200

    Add files via upload

---
+++ CS-Lab-1/CS-1-1/CS-1-1/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace CS_1_1
+{
+    class Program
+    {
+        static int lettersNumber;
+
+        static string ReadFile(string path)
+        {
+            string text ;
+
+            using(StreamReader st = new StreamReader(path, Encoding.Default))
+            {
+                text = st.ReadToEnd();
+            }
+
+            return text;
+        }
+
+        static Dictionary<char, double> Frequency(string text)
+        {
+
+            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";//"абвгґдеєжзиіїйклмнопрстуфхцчшщьюя";
+            Dictionary<char, double> frequency = new Dictionary<char, double>(33);
+            Dictionary<char, double> tempFrequency = new Dictionary<char, double>(33);
+            foreach (char c in alphabet)
+            {
+                frequency.Add(c, 0);
+                tempFrequency.Add(c, 0);
+            }
+            char tempChar;
+            int symbolNumbers = 0;
+            foreach(char c in text)
+            {
+                tempChar = c;//char.ToLower(c);
+                if (!alphabet.Contains(tempChar)) continue;
+                symbolNumbers++;
+                tempFrequency[tempChar]++;
+            }
+            lettersNumber = symbolNumbers;
+            foreach(KeyValuePair<char,double> a in tempFrequency)
+            {
+                frequency[a.Key] = tempFrequency[a.Key] / symbolNumbers;
+                Console.WriteLine("The frequency of the symbol \'{0}\' is {1}",a.Key, frequency[a.Key]);
+            }
+
+            return frequency;
+        }
+
+        static double Entropy(Dictionary<char, double> frequency)
+        {
+            double entropy = 0;
+            foreach(KeyValuePair<char,double> a in frequency)
+            {
+                if (a.Value == 0) continue;
+                entropy += a.Value * Math.Log(1/a.Value, 2);
+            }
+            Console.WriteLine("Entropy: " + entropy);
+            return entropy;
+        }
+
+        static double NumberOfInformation(double entropy, string path)
+        {
+            double numberInfo = 0;
+
+            numberInfo = entropy / 8 * lettersNumber;
+            FileInfo f = new FileInfo(path);
+            Console.WriteLine("File size: " + f.Length);
+            Console.WriteLine("Number of informations: " + numberInfo);
+
+            return numberInfo;
+        }
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Enter file path:");
+            string[] paths = { @"C:\__study\text1-rar-base64.txt", @"C:\__study\text2-rar-base64.txt", @"C:\__study\text3-rar-base64.txt" };
+
+            for (int i = 0; i < paths.Length; i++)
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine("File " + (i + 1) + "\n");
+
+                string path = paths[i];
+                string text = ReadFile(path);
+                Dictionary<char, double> frequency = Frequency(text);
+                double entropy = Entropy(frequency);
+                NumberOfInformation(entropy, path);
+
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
---
+++ CS-Lab-1/CS-1-2/CS-1-2/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CS_1_2
+{
+    class Program
+    {
+        static byte[] ReadFile(string path)
+        {
+            List<byte> arr = new List<Byte>();
+            using (BinaryReader br = new BinaryReader(File.Open(path, FileMode.Open)))
+            {
+                while(br.BaseStream.Position != br.BaseStream.Length)
+                {
+                    arr.Add(br.ReadByte());
+                    //Console.WriteLine(arr[arr.Count - 1]);
+                }
+            }
+            byte[] result = arr.ToArray();
+            return result;
+        }
+
+        static void Base64(byte[] byteCode)
+        {
+            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+            string[] binaryCode8 = new string[byteCode.Length];
+            string temnCode = "";
+            int numberBit = 0;
+            for (int i = 0; i < byteCode.Length; i++) 
+            {
+                int temp = byteCode[i];
+                temnCode = "";
+                numberBit = 0;
+                while ((temp > 1) || (numberBit != 8)) 
+                {
+                    temnCode += temp % 2;
+                    temp = temp / 2;
+                    numberBit++;
+                }
+                char[] c = temnCode.ToCharArray();
+                Array.Reverse(c);
+                binaryCode8[i] = new string(c);
+            }
+            //foreach(string a in binaryCode8)
+            //{
+            //    Console.WriteLine(a);
+            //}
+
+            List<string> binaryCode6 = new List<string>();
+
+            for(int i = 0; i < binaryCode8.Length; i++)
+            {
+                switch (i % 3)
+                {
+                    case 0:
+                        binaryCode6.Add(binaryCode8[i].Substring(0, 6));
+                        if(i+1 == binaryCode8.Length)
+                        {
+                            binaryCode6.Add(binaryCode8[i].Substring(6, 2) + "0000");
+                        }
+                        break;
+                    case 1:
+                        binaryCode6.Add(binaryCode8[i - 1].Substring(6, 2) + binaryCode8[i].Substring(0, 4));
+                        if (i + 1 == binaryCode8.Length)
+                        {
+                            binaryCode6.Add(binaryCode8[i].Substring(4, 4) + "00");
+                        }
+                        break;
+                    case 2:
+                        binaryCode6.Add(binaryCode8[i - 1].Substring(4, 4) + binaryCode8[i].Substring(0, 2));
+                        binaryCode6.Add(binaryCode8[i].Substring(2, 6));
+                        break;
+                }
+            }
+
+            //foreach (string a in binaryCode6)
+            //    Console.WriteLine(a);
+
+            string answer = "";
+            for(int i = 0; i < binaryCode6.Count; i++)
+            {
+                int temp = 0;
+                int q = 5;
+                for(int j = 0; j < 6; j++)
+                {
+                    temp += (int)Math.Pow(2, q) * int.Parse(binaryCode6[i][j].ToString());
+                    q--;
+                }
+                //Console.Write(temp + " ");
+                answer += alphabet[temp];
+            }
+            if (answer.Length % 4 == 3) answer += "=";
+            else if (answer.Length % 4 == 2) answer += "==";
+            Console.WriteLine(answer);
+        }
+
+        static void Main(string[] args)
+        {
+            byte[] byteCode = ReadFile(@"C:\__study\text1.rar");
+            Base64(byteCode);
+            Console.WriteLine("\n\n");
+            byteCode = ReadFile(@"C:\__study\text2.rar");
+            Base64(byteCode);
+            Console.WriteLine("\n\n");
+            byteCode = ReadFile(@"C:\__study\text3.rar");
+            Base64(byteCode);
+        }
+    }
+}
